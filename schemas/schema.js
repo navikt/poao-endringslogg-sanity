0d91919649171444f1eb3925c89c27d0ba30fb63
Mathias S. Müller <mathias.sundt.muller@nav.no>
2023-03-22 12:38:55 +0100

    Fiks error og rydd opp

--- schemas/schema.js
+++ schemas/schema.js
@@ -6,9 +6,9 @@ import {mrAdminflateEndringsloggSchema} from "./mrAdminflateEndringslogg";
 export const schemas = [
   // The following are document types which will appear
   // in the studio.
-    endringsloggSchema,
-    oversiktAlert,
-    mrAdminflateEndringsloggSchema,
+  endringsloggSchema,
+  oversiktAlert,
+  mrAdminflateEndringsloggSchema,
   // When added to this list, object types can be used as
   // { type: 'typename' } in other document schemas
   blockContent,
