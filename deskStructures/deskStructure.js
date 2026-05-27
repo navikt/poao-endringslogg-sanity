ecc1440f3d8d1b527d27b98ea5ca141cabde38b5
Mathias S. Müller <mathias.sundt.muller@nav.no>
2026-05-27 09:35:42 +0200

    Fjern mulighetsrommet fra dette prosjektet (aldri vært brukt)

--- deskStructures/deskStructure.js
+++ deskStructures/deskStructure.js
@@ -2,15 +2,7 @@ export const structure = (S) => {
     return (S.list()
             .title('Innhold')
             .items([
-                ...S.documentTypeListItems()
-                    .filter((listItem) =>
-                        !["endringslogg_mr-adminflate"].includes(listItem.getId())
-                    ),
-                S.divider(),
-                ...S.documentTypeListItems()
-                    .filter((listItem) =>
-                        ["endringslogg_mr-adminflate"].includes(listItem.getId())
-                    ),
+                ...S.documentTypeListItems(),
             ])
     )
 }
--- schemas/schema.js
+++ schemas/schema.js
@@ -1,14 +1,12 @@
 import { blockContent } from "./blockContent";
 import {endringsloggSchema} from "./endring";
 import {oversiktAlert} from "./oversiktAlert";
-import {mrAdminflateEndringsloggSchema} from "./mrAdminflateEndringslogg";
 
 export const schemas = [
   // The following are document types which will appear
   // in the studio.
   endringsloggSchema,
   oversiktAlert,
-  mrAdminflateEndringsloggSchema,
   // When added to this list, object types can be used as
   // { type: 'typename' } in other document schemas
   blockContent,
